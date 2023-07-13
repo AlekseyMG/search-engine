876474dc8417f523bfc89b40640a01a25c39d63f
Aleksey Serebryakov <vinni-pux@yandex.ru>
2023-07-13 17:59:56 +0300

    startIndexing - удаление из таблиц работает

--- src/main/java/searchengine/services/IndexingServiceImpl.java
+++ src/main/java/searchengine/services/IndexingServiceImpl.java
@@ -13,13 +13,15 @@ import searchengine.repository.PageRepository;
 import searchengine.repository.SiteRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
 public class IndexingServiceImpl implements IndexingService {
 //брать из конфигурации приложения список сайтов и по каждому сайту:
 
-//удалять все имеющиеся данные по этому сайту (записи из таблиц
+    //удалять все имеющиеся данные по этому сайту (записи из таблиц
 //site и page);
 //○ создавать в таблице site новую запись со статусом INDEXING;
 //○ обходить все страницы, начиная с главной, добавлять их адреса,
@@ -30,32 +32,47 @@ public class IndexingServiceImpl implements IndexingService {
 //○ если произошла ошибка и обход завершить не удалось, изменять
 //статус на FAILED и вносить в поле last_error понятную
 //информацию о произошедшей ошибке
+    private String pageUrl;
     private final SitesList sites;
     @Autowired
     private final PageRepository pageRepository;
     @Autowired
     private final SiteRepository siteRepository;
+
     @Override
     public DefaultResponse startIndexing() {
-        sites.getSites().forEach(site -> System.out.println(site.getUrl()));
-//        sites.getSites().get(0).getUrl();
-//        Site siteForDelete = siteRepository.findAll().get(0);
+        sites.getSites()
+                .stream()
+                .map(searchengine.config.Site::getUrl)
+                .forEach(url -> {
+                    pageRepository.findAll().forEach(page -> {
+                        if (page.getPath().contains(url)) {
+                            pageRepository.deleteById(page.getId());
+                        }
+                    });
+                    siteRepository.findAll().forEach(site -> {
+                        if (site.getUrl().contains(url)) {
+                            siteRepository.deleteById(site.getId());
+                        }
+                    });
+                });
 
-        Site site = new Site();
-        site.setName(sites.getSites().get(0).getName());
-        site.setUrl(sites.getSites().get(0).getUrl());
-        site.setStatus(StatusType.INDEXED);
-        site.setLastError("vse norm");
-        site.setStatusTime(LocalDateTime.now());
-        Page page = new Page();
-        page.setCode(200);
-        page.setSite(site);
-        page.setPath(site.getUrl());
-        page.setContent("fignya");
-        pageRepository.saveAndFlush(page);
 
-//        return new DefaultResponse();
-        return new ErrorResponse("ошика 111");
+//        Site site = new Site();
+//        site.setName(sites.getSites().get(0).getName());
+//        site.setUrl(sites.getSites().get(0).getUrl());
+//        site.setStatus(StatusType.INDEXED);
+//        site.setLastError("vse norm");
+//        site.setStatusTime(LocalDateTime.now());
+//        Page page = new Page();
+//        page.setCode(200);
+//        page.setSite(site);
+//        page.setPath(site.getUrl());
+//        page.setContent("fignya");
+//        pageRepository.saveAndFlush(page);
+
+        return new DefaultResponse();
+//        return new ErrorResponse("ошика 111");
 
     }
 }
